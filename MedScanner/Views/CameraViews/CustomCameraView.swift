15c1a4225e3b43d086919b612d7e8f5d192168e4
Giovanni Palusa <giovanni@palusa.se>
2021-10-16 11:26:34 +0200

    added camera view and photo capture

--- MedScanner/Views/CameraViews/CustomCameraView.swift
+++ MedScanner/Views/CameraViews/CustomCameraView.swift
@@ -8,6 +8,32 @@
 import SwiftUI
 
 struct CustomCameraView: View {
+    var customCameraRepresentable: CustomCameraRepresentable
+    var imageCompletion: ((UIImage) -> Void)
+
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                let frame = CGRect(x: 0, y: 0, width: geometry.size.width, height: geometry.size.height - 100)
+                cameraView(frame: frame)
+
+                HStack {
+                    CameraControlsView(captureButtonAction: { [weak customCameraRepresentable] in
+                        customCameraRepresentable?.takePhoto()
+                    })
+                }
+            }
+        }
+    }
+
+    private func cameraView(frame: CGRect) -> CustomCameraRepresentable {
+        customCameraRepresentable.cameraFrame = frame
+        customCameraRepresentable.imageCompletion = imageCompletion
+        return customCameraRepresentable
+    }
+}
+
+struct CameraControlsView: View {
     var captureButtonAction: (() -> Void)
 
     var body: some View {
@@ -15,11 +41,5 @@ struct CustomCameraView: View {
             .onTapGesture {
                 captureButtonAction()
             }
-    }
-}
-
-struct CustomCameraView_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomCameraView(captureButtonAction: {})
     }
 }
