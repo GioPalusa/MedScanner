20aab0c45ff48b2d4ae82d21681c408289ac841e
Mert Avci <mert.avci@knowit.se>
2021-10-16 14:13:54 +0200

    Auto stash before rebase of "origin/main"
    expendiable cells

--- MedScanner/Views/MedicineList.swift
+++ MedScanner/Views/MedicineList.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct MedicineList: View {
+    
+    @State private var selection: Set<Medicine> = []
+    
+    var medicineList = [
+        Medicine(id: UUID(), name: "Alvedon", description: "Alvedon     description"),
+        Medicine(id: UUID(), name: "Ipren", description: "Ipren description"),
+        Medicine(id: UUID(), name: "Pamol", description: "Pamol description")]
+    
     var body: some View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: true) {
                 LazyVStack {
-                    ForEach(1...100, id: \.self) { value in
-                        MedicineCell()
-                    }.frame(width: .infinity, height: 100)
+                    ForEach(medicineList) { medicine in
+                        MedicineCell(isExpanded: self.selection.contains(medicine))
+                            .onTapGesture { selectDeselect(medicine) }
+                    }
                 }
             }
             .navigationTitle("Min lista")
+        }
+    }
+    
+    func selectDeselect(_ medicine: Medicine) {
+        if selection.contains(medicine) {
+            selection.remove(medicine)
+        } else {
+            selection.insert(medicine)
         }
     }
 }
